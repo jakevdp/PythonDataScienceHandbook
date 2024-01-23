85b7138fd7c30d493fbf562874893966ea338528
damian gacic <84860151+DamianGacic@users.noreply.github.com>
2024-01-23 20:33:08 +0000

    mostly notes about time and memory profiling functions

---
+++ notebooks_v1/mprun_demo.py
@@ -0,0 +1,7 @@
+def sum_of_lists(N):
+    total = 0
+    for i in range(5):
+        L = [j ^ (j >> i) for j in range(N)]
+        total += sum(L)
+        del L # remove reference to L
+    return total
